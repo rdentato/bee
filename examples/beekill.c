1dd330ce3196189fefc287d096aa0f2fc94256dc
rdentato <rdentato@gmail.com>
2023-03-13 13:51:59 +0100

    Moved examples.

--- examples/beekill.c
+++ examples/beekill.c
@@ -26,13 +26,17 @@ int main(int argc, char *argv[])
     nxt1->id = 1;
     nxt2->id = 2;
 
-    beereset(nxt1);
+    beereset(nxt1); // nothing will happen as the bee never took off!
 
     // Iterate over the items
     while (beefly(nxt1) | beefly(nxt2)) {
       if (nxt2->n == 5) beekill(nxt2);
-      if (reset && nxt1->n == 7) { beereset(nxt1); reset = 0;}
+      if (reset && nxt1->n == 7) {
+        beereset(nxt1); // This time it will execute the cleanupcode and will start from 0
+        reset = 0;
+      } 
     }
+    
     beefree(nxt1);
     beefree(nxt2);
 }
--- makefile
+++ makefile
@@ -1,24 +1,18 @@
 #  (C) by Remo Dentato (rdentato@gmail.com)
 #  SPDX-License-Identifier: MIT
 
-CFLAGS=-O2 -Wall
-
-EXAMPLES=iter prodcons fib iter_sleep prodcons_hive beekill
-
 # implicit rules
 MAKEFLAGS += --no-builtin-rules
 
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
-
-%: %.o
-	$(CC) $(CFLAGS) $< -o $* 
+%:
+	cd examples; make $*
 
 .PRECIOUS: %.o
 
 # targets
 
-all: $(EXAMPLES)
+all:
+	cd examples; make all
 
 clean:
-	rm -f $(EXAMPLES) $(EXAMPLES:=.exe) $(EXAMPLES:=.o)
+	cd examples; make clean
